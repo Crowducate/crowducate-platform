38becfb7d254b984bd03d5b6ed142be1cb3b2901
nizyev <illya.nizyev@gmail.com>
2015-01-22 15:08:25 +0200

    descr text added

--- seo.js
+++ seo.js
@@ -1,9 +1,9 @@
 Meteor.startup(function() {
   if(Meteor.isClient) {
     SEO.config({
-      title: 'Project Title',
+      title: 'Crowducate-next',
       meta: {
-        'description': 'A project by Differential.io'
+        'description': 'A project by Meteorcrowd'
       },
       og: {
         'image': Meteor.absoluteUrl('share-image.png')
