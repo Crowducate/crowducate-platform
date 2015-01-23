627a929f0ab202e04e0fcd363b9eb0f558055c1f
nizyev <illya.nizyev@gmail.com>
2015-01-23 11:04:27 +0200

    grid of learning material + demo content

--- routes.js
+++ routes.js
@@ -39,12 +39,18 @@ Router.map(function() {
     this.route('learn', {
         path: '/learn',
         loginRequired: 'entrySignIn',
-        //        waitOn: function() {
-        //            return this.subscribe("items");
-        //        },
-        //        data: {
-        //            items: Items.find({})
-        //        },
+        //                waitOn: function() {
+        //                    return this.subscribe("items");
+        //                },
+        data: {
+            resources: [
+                { title: "Title 1", description: "Descr 1", progress: "10%", author: "Smb1", editors: "1" },
+                { title: "Title 2", description: "Descr 2", progress: "20%", author: "Smb2", editors: "2" },
+                { title: "Title 3", description: "Descr 3", progress: "30%", author: "Smb3", editors: "3" },
+                { title: "Title 4", description: "Descr 4", progress: "40%", author: "Smb4", editors: "4" },
+                { title: "Title 5", description: "Descr 5", progress: "50%", author: "Smb5", editors: "5" }
+            ]
+        },
         onAfterAction: function() {
             SEO.set({
                 title: 'Learn | ' + SEO.settings.title
