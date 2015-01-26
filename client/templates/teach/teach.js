be7ce1c3e70e6a49ceff799687491d3a5d3f6a7b
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-01-26 20:49:49 +0200

    Changed to resource object. Added selectize.

--- client/templates/teach/teach.js
+++ client/templates/teach/teach.js
@@ -1,16 +1,34 @@
 Template.teach.events({
     'click #addResource': function(event, template){
         event.preventDefault();
+
+        // create an empty resource container
+        var resource = {}
+
         //getting a value of each field
-        var title = template.find('#resourceTitle').value;
-        var keywords = template.find('#resourceKeywords').value;
-        var author = template.find('#authorName').value;
-        var description = template.find('#descriptionText').value;
+        resource.title = template.find('#resourceTitle').value;
+        resource.author = template.find('#authorName').value;
+        resource.keywords = template.find('#resourceKeywords').value;
+        resource.published = template.find('#resourcePublished').value;
+        resource.description = template.find('#descriptionText').value;
 
         //adding resource to collection
-        Resources.insert({title: title, authoredBy: author, description: description, keywords: keywords});
+        Resources.insert(resource);
 
         // Redirect to the learn page, for now
         Router.go('learn');
     }
-})
+});
+
+Template.teach.rendered = function() {
+    $('#resourceKeywords').selectize({
+        delimiter: ',',
+        persist: false,
+        create: function(input) {
+            return {
+                value: input,
+                text: input
+            }
+        }
+    });
+};
