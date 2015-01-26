fbc639ccd199891b941bbc3af7ead75b3d598213
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-01-26 21:20:10 +0200

    Added resource object. Convert keywords to array.

--- client/templates/teach/teach.js
+++ client/templates/teach/teach.js
@@ -1,16 +1,19 @@
 Template.teach.events({
     'click #addResource': function(event, template){
+        // prevent default button submit
         event.preventDefault();
 
         // create an empty resource container
         var resource = {}
 
-        //getting a value of each field
-        resource.title = template.find('#resourceTitle').value;
-        resource.author = template.find('#authorName').value;
-        resource.keywords = template.find('#resourceKeywords').value;
-        resource.published = template.find('#resourcePublished').value;
-        resource.description = template.find('#descriptionText').value;
+        // Getting form field values
+        // Converting keywords to array
+        // all other values are strings
+        resource.title = template.find('#resourceTitle').value; // string
+        resource.author = template.find('#authorName').value; // string
+        resource.keywords = template.find('#resourceKeywords').value.split(','); // split keywords to array
+        resource.published = template.find('#resourcePublished').value; // string
+        resource.description = template.find('#descriptionText').value; // string
 
         //adding resource to collection
         Resources.insert(resource);
