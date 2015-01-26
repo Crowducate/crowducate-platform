2ba1446c2eea287fe3f80b31dc077ad4a1686c79
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-01-26 14:59:06 +0200

    Fixed recursion issue. Added calls to .value

--- client/templates/teach/teach.js
+++ client/templates/teach/teach.js
@@ -2,10 +2,11 @@ Template.teach.events({
     'click #addResource': function(event, template){
         event.preventDefault();
         //getting a value of each field
-        var title = template.find('#resourceTitle');
-        var keywords = template.find('#resourceKeywords');
-        var author = template.find('#authorName');
-        var description = template.find('#descriptionText');
+        var title = template.find('#resourceTitle').value;
+        var keywords = template.find('#resourceKeywords').value;
+        var author = template.find('#authorName').value;
+        var description = template.find('#descriptionText').value;
+
         //adding resource to collection
         Resources.insert({title: title, author: author, description: description, keywords: keywords});
     }
