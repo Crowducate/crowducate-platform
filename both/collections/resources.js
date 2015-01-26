c0329955dafde1d7517140dd5ab64760fc2d995f
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-01-26 20:48:07 +0200

    removed schema

--- both/collections/resources.js
+++ both/collections/resources.js
@@ -1,17 +1,1 @@
 Resources = new Mongo.Collection('resources');
-
-//Resources.attachSchema(new SimpleSchema({
-//    title: {
-//        type: String,
-//        label: "Title",
-//        max: 200
-//    },
-//    keywords: {
-//        type: [String],
-//        label: "Keywords"
-//    },
-//    published: {
-//        type: Boolean,
-//        label: "Published"
-//    }
-//}));
