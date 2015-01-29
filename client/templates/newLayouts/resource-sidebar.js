9316fff3d076a3922c130fb713243a7fe584834c
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-01-29 11:25:35 +0200

    Refactor resource page.

---
+++ client/templates/newLayouts/resource-sidebar.js
@@ -0,0 +1,32 @@
+Template.resourceSidebar.helpers({
+    'sections': function () {
+        var sections = [
+            {
+                "title": "Birds",
+                "lessons": [
+                    {
+                        "title": "Nest building"
+                    },
+                    {
+                        "title": "Feeding"
+                    },
+                    {
+                        "title": "Flight"
+                    }
+                ]
+            },
+            {
+                "title": "Bees",
+                "lessons": [
+                    {
+                        "title": "Hive building"
+                    },
+                    {
+                        "title": "Brood"
+                    },
+                ]
+            }
+        ];
+        return sections;
+    }
+});
