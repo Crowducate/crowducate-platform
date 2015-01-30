4c4c48ba5379ff5f2bb4b0e3c152bdae5c6a1aac
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-01-30 19:01:07 +0200

    Working add lesson event, get name from template.

--- client/templates/newLayouts/sidebar-section.js
+++ client/templates/newLayouts/sidebar-section.js
@@ -1,16 +1,26 @@
 Template.sidebarSection.events({
     'click .add-page': function (event, template) {
-        // set up a demonstration lesson with title
-        var newLesson = {"title": "New lesson"};
+        // get the value of the input field
+        var lessonTitle = template.find('.page-name').value;
 
-        // get the template data for use in this funciton
-        var resourceData = template.data;
+        // Make sure the lesson title contains one or more characters
+        // and add lesson to section
+        if (lessonTitle.length) {
+            // set up a demonstration lesson with title
+            var newLesson = {"title": lessonTitle};
 
-        // add the example lesson to the lessons object
-        resourceData.lessons.push(newLesson);
+            // get the template data for use in this funciton
+            var resourceData = template.data;
 
-        // Set the reactive variable to include the new lesson
-        // sections is defined in the resource-sidebar.js
-        sections.set(resourceData);
+            // add the example lesson to the lessons object
+            resourceData.lessons.push(newLesson);
+
+            // Set the reactive variable to include the new lesson
+            // sections is defined in the resource-sidebar.js
+            sections.set(resourceData);
+
+            // Reset the template field
+           $('.page-name').val('');
+        }
     }
 });
