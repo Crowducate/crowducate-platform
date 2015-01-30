4cb96229af54d2f65f00ae10dab6f9e024739c8b
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-01-30 18:40:05 +0200

    Removed resource data; added reactive variable.

--- client/templates/newLayouts/resource-sidebar.js
+++ client/templates/newLayouts/resource-sidebar.js
@@ -1,32 +1,9 @@
 Template.resourceSidebar.helpers({
     'sections': function () {
-        var sections = [
-            {
-                "title": "Birds",
-                "lessons": [
-                    {
-                        "title": "Nest building"
-                    },
-                    {
-                        "title": "Feeding"
-                    },
-                    {
-                        "title": "Flight"
-                    }
-                ]
-            },
-            {
-                "title": "Bees",
-                "lessons": [
-                    {
-                        "title": "Hive building"
-                    },
-                    {
-                        "title": "Brood"
-                    },
-                ]
-            }
-        ];
-        return sections;
+        // create a reactive variable based on the template data
+        sections = new ReactiveVar(this.sections);
+
+        // return the reactive variable
+        return sections.get();
     }
 });
