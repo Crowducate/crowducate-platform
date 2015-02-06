1c8ddfb8ea9057b47879883fad19ed1a24bd7938
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-02-06 15:14:33 +0200

    Added course js.

--- client/templates/course/course.js
+++ client/templates/course/course.js
@@ -1,16 +1,16 @@
 Template.course.events({
-    'click  #edit-resource': function () {
+    'click  #edit-course': function () {
         // in edit mode,
         // add rich text editor
         if (Session.get('editMode')) {
-            $('.resource-page-content').summernote();
+            $('.course-page-content').summernote();
         }
     },
-    'click #cancel-edit-resource': function () {
+    'click #cancel-edit-course': function () {
         // not in edit mode,
         // remove rich text editor
         if (!Session.get('editMode')) {
-            $('.resource-page-content').destroy();
+            $('.course-page-content').destroy();
         }
     }
 });
