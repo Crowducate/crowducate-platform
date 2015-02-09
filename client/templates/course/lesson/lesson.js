a9af833b8a27d81e0a7471ba4bb6e3ed69893cc5
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-02-09 12:24:55 +0200

    Added focus variable and events.

--- client/templates/course/lesson/lesson.js
+++ client/templates/course/lesson/lesson.js
@@ -1,7 +1,28 @@
+var courseTitleFocus = new ReactiveVar(false);
+
 Template.lesson.helpers({
     editClass: function(){
         if (Session.get('editMode')) {
             return 'course-page-content-edit';
         }
+    },
+    'contentEditable': function () {
+        if (Session.get('editMode')) {
+            console.log("Content editable");
+            return true;
+        } else {
+            return false;
+        }
     }
-})
+});
+
+Template.lesson.events({
+    'focus #course-title': function (event, template) {
+        console.log("Course title focus.");
+        courseTitleFocus.set(true);
+    },
+    'blur #course-title': function (event, template) {
+        console.log("Course title blur.");
+        courseTitleFocus.set(false);
+    }
+});
