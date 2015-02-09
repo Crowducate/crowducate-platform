af469e4cbeddbefb123dfa4ff723dabe60048f69
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-02-09 16:50:28 +0200

    Course info controller.

---
+++ both/controllers/courseInfo.js
@@ -0,0 +1,20 @@
+CourseInfoController = AppController.extend({
+  waitOn: function() {
+        // Wait for the course to be available
+        return this.subscribe('singleCourse', this.params._id);
+    },
+    data: function () {
+        // Return the course
+        return Courses.findOne();
+    },
+    onAfterAction: function () {
+        // Get the course ID from the URL parameters
+        var courseID = this.params._id;
+
+        // Get the course from the database
+        var course  = Courses.find(courseID).fetch()[0]; // select the zeroeth array item
+
+        // Set the site title for SEO
+        Meta.setTitle(course.title);
+    }
+});
