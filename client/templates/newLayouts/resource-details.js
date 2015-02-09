7ccc6eb101ca7f178727444bf85108f2feab7380
nizyev <illya.nizyev@gmail.com>
2015-02-09 21:37:12 +0200

    drag and drop basics added

--- client/templates/newLayouts/resource-details.js
+++ client/templates/newLayouts/resource-details.js
@@ -4,4 +4,15 @@ Template.testresourceDetails.helpers({
             return 'resource-page-content-edit';
         }
     }
-})
+});
+
+Template.testresourceDetails.rendered = function(){
+    // if editMode is on
+    var sortablediv = document.getElementById('sortable-divs');
+    var sortable = Sortable.create(sortablediv, {
+        animation: 100,
+        ghostClass: 'ghost'
+    });
+}
+
+
