115c8b8d6f546bced9a44db9e7c4102fa670c4a8
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-02-11 19:02:24 +0200

    Return active lesson from db.

--- client/templates/course/lesson/lesson.js
+++ client/templates/course/lesson/lesson.js
@@ -1,5 +1,14 @@
 Template.lesson.helpers({
+    'activeLesson': function () {
+        // Get the lesson ID from session
+        // TODO: see if this can be done via reactive var
+        var lessonID = Session.get('activeLesson');
 
+        // Get the lesson from DB
+        // selecting the zeroth array item
+        // TODO: see if findOne would be better here.
+        var lesson = Lessons.find(lessonID).fetch()[0];
+
+        return lesson;
+    }
 });
-
-
