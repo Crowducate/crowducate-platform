5843294bc7463e061908b96cc7117870bf10e237
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-02-11 11:20:20 +0200

    Add section to course in db.

--- client/templates/course/sidebar/add-section.js
+++ client/templates/course/sidebar/add-section.js
@@ -1,5 +1,25 @@
 Template.addSection.events({
     'click #add-section-button': function (event, template) {
-        console.log('Adding section to course.');
+        // Get current sections, before adding new section
+        if (this.sections) {
+            // If sections exists, use them
+            var sections = this.sections;
+        } else {
+            // otherwise create empty sections array
+            var sections = [];
+        }
+
+
+        // Get the title of new section
+        var title = template.find('#section-title').value;
+
+        // Set section title in new section object
+        var newSection = {'title': title};
+
+        // Add section to end of existing sections
+        sections.push(newSection);
+
+        // Update the course with new section
+        Courses.update(this._id, {$set: {'sections': sections}});
     }
 });
