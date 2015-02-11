16c80e2a385e2d3af4a8a538cff76b78f0714010
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-02-11 19:19:31 +0200

    Added example edit functionality.

--- client/templates/course/course.js
+++ client/templates/course/course.js
@@ -2,15 +2,20 @@ Template.course.events({
     'click  #edit-course': function () {
         // in edit mode,
         // add rich text editor
+        // to lesson text
         if (Session.get('editMode')) {
-            $('.course-page-content').summernote();
+            $('.lesson-text').summernote({
+                // TODO: add more config parameters as needed
+                height: 300
+            });
         }
     },
     'click #cancel-edit-course': function () {
         // not in edit mode,
         // remove rich text editor
+        // from lesson text
         if (!Session.get('editMode')) {
-            $('.course-page-content').destroy();
+            $('.lesson-text').destroy();
         }
     }
 });
