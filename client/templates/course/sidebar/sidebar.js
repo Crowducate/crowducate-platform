ca6230070fcb1ee1ab010aff0f9871cec67a908e
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-02-11 11:54:07 +0200

    Removed unnecessary template helper.

--- client/templates/course/sidebar/sidebar.js
+++ client/templates/course/sidebar/sidebar.js
@@ -1,11 +1,4 @@
 Template.courseSidebar.helpers({
-    'sections': function () {
-        // create a reactive variable based on the template data
-        sections = new ReactiveVar(this.sections);
-
-        // return the reactive variable
-        return sections.get();
-    },
     'editMode': function () {
         return Session.get('editMode');
     }
