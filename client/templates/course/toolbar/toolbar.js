3ddf05c7a6acb9625934ac400b27f54b6f3d53a0
nizyev <illya.nizyev@gmail.com>
2015-02-12 14:57:52 +0200

    inline editable added for sidebar elements

--- client/templates/course/toolbar/toolbar.js
+++ client/templates/course/toolbar/toolbar.js
@@ -1,9 +1,13 @@
 Template.courseToolbar.events({
     'click #edit-course':function(event, template){
         Session.set('editMode', true);
+        $.fn.editable.defaults.mode = 'inline';
+        $('.lesson-link').editable();
+        $('#section-title').editable();
     },
     'click #cancel-edit-course': function () {
         Session.set('editMode', false);
+        $('#section-title').editable('destroy');
     }
 });
 
