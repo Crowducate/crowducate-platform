77f6b0b242d20fb92d1ac607da0cb269681d2fea
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-02-13 08:27:11 +0200

    Add Course IDs array to each lesson.

--- client/templates/course/sidebar/section/add-lesson/add-lesson.js
+++ client/templates/course/sidebar/section/add-lesson/add-lesson.js
@@ -3,8 +3,14 @@ Template.sectionAddLesson.events({
         // Get lesson name
         var lessonName = template.find(".lesson-name").value;
 
+        // Get course ID from parent template
+        var courseID = Template.parentData()._id;
+
         // Create temporary lesson object
-        var lessonObject = {'name': lessonName};
+        var lessonObject = {
+            'name': lessonName,
+            'courseIDs': [courseID] // add course ID to lesson object, for collection helper
+        };
 
         // Add lesson to database,
         // getting lesson ID in return
@@ -18,9 +24,6 @@ Template.sectionAddLesson.events({
 
         // Get course sections array from parent template
         var courseSections = Template.parentData().sections;
-
-        // Get course ID from parent template
-        var courseID = Template.parentData()._id;
 
         // Save course.lessonIDs array to database
         Courses.update(courseID, {$set: {"sections": courseSections}});
