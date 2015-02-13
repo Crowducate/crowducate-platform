7ececd1c3d3a785319b4bf18dfe3a4385f54a08c
nizyev <illya.nizyev@gmail.com>
2015-02-13 19:35:09 +0200

    code cleaning

--- client/templates/course/course.js
+++ client/templates/course/course.js
@@ -1,11 +1,11 @@
-Template.course.events({
-    'click  #edit-course': function () {
-        // set edit mode variable to true
-    },
-    'click #cancel-course-edit': function () {
-        // set edit mode variable to false
-    }
-});
+//Template.course.events({
+//    'click  #edit-course': function () {
+//        // set edit mode variable to true
+//    },
+//    'click #cancel-course-edit': function () {
+//        // set edit mode variable to false
+//    }
+//});
 
 Template.course.rendered = function () {
     // set default mode for x-editable
