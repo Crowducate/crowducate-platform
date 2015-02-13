09ae006c71c76bca453d4316c3f983065bc82ec9
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-02-13 08:26:30 +0200

    Added lessons helper.

--- both/collections/courses.js
+++ both/collections/courses.js
@@ -1,1 +1,8 @@
 Courses = new Mongo.Collection("courses");
+
+Courses.helpers({
+    'lessons': function () {
+        // return all lessons related to course
+        return Lessons.find({'courseIDs': this._id});
+    }
+});
