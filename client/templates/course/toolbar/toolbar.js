656d64d67c040af3e580ab1cc1a880b6153b4031
nizyev <illya.nizyev@gmail.com>
2015-02-13 19:35:49 +0200

    renaming template, cancel or save butons alert added

--- client/templates/course/toolbar/toolbar.js
+++ client/templates/course/toolbar/toolbar.js
@@ -1,19 +1,38 @@
 Template.courseToolbar.events({
     'click #edit-course':function(event, template){
+        //Edit Mode is on
         Session.set('editMode', true);
+        //enables inline-editing
+        //TODO: think about better place
         $('.lesson-link').editable();
         $('#section-title').editable();
         $('#course-title').editable();
     },
-    'click #cancel-course-edit': function () {
+    'click #save-course-edit': function () {
+        //Edit Mode is off
         Session.set('editMode', false);
+        //disables inline-editing
+        $('.lesson-link').editable('destroy');
         $('#section-title').editable('destroy');
         $('#course-title').editable('destroy');
-    }
-});
-
-Template.courseToolbar.helpers({
-    'editMode': function () {
-        return Session.get('editMode');
+    },
+    'click #cancel-course-edit': function () {
+        //confirmation alert
+        //if yes,
+        if (confirm('Are you sure you want to cancel, you will loose your last changes?')) {
+            //revert changes: editMode is false
+            Session.set('editMode', false);
+            //clear entered data into rich-text-editor
+            //clear entered data into x-editable fields
+            //disables inline-editing
+            $('.lesson-link').editable('disable');
+            $('.lesson-link').editable('destroy');
+            $('#section-title').editable('destroy');
+            $('#course-title').editable('destroy');
+            //if no,
+        } else {
+            //do nothing set session true
+            Session.set('editMode', true);
+        }
     }
 });
