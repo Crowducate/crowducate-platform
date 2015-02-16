c6eb79d0b283f5b29bba5b128866baa0c7b4b82b
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-02-16 17:33:53 +0200

    Moved inline editor helper functions.

--- client/templates/course/course.js
+++ client/templates/course/course.js
@@ -15,3 +15,31 @@ Template.course.rendered = function () {
         '<button type="submit" class="btn btn-success editable-submit btn-mini"><i class="fa fa-check"></i></button>' +
         '<button type="button" class="btn btn-danger editable-cancel"><i class="fa fa-times"></i></button>';
 }
+
+/*
+* Enable sidebar inline editors
+* used in various child-template events
+* such as template rendered, cancel edit, etc
+*/
+enableSidebarInlineEditors = function () {
+    //enables inline-editing
+    //TODO: think about better place
+    $('.lesson-link').editable({
+        // Make sure text doesn't display twice after saving
+        display: false
+    });
+    $('.section-title').editable();
+    $('#course-title').editable();
+};
+
+/*
+* Disable sidebar inline editors
+* used in various child-template events
+* such as template rendered, cancel edit, etc
+*/
+disableSidebarInlineEditors = function () {
+    //disables inline-editing
+    $('.lesson-link').editable('destroy');
+    $('.section-title').editable('destroy');
+    $('#course-title').editable('destroy');
+};
