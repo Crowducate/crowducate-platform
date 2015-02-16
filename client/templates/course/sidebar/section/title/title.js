67c56891df3874e8e51a5c4506fef22354461e27
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-02-16 18:40:24 +0200

    Section title template event update Course.

---
+++ client/templates/course/sidebar/section/title/title.js
@@ -0,0 +1,18 @@
+Template.sectionTitle.events({
+    'click .editable-submit': function (event, template) {
+        // Get the new section title from template
+        var newSectionTitle = template.find('input').value;
+
+        // override current title in template data
+        this.title = newSectionTitle;
+
+        // Get current sections from parent template data
+        var sections = Template.parentData().sections;
+
+        // Get course ID from parent template data
+        var courseID = Template.parentData()._id;
+
+        // Update course sections
+        Courses.update(courseID, {$set: {'sections': sections}});
+    }
+});
