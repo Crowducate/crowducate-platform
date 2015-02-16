174599ff6da6494c35100f8d9b29191cdaedbbdc
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-02-16 17:40:33 +0200

    Clean-up.

--- client/templates/course/course.js
+++ client/templates/course/course.js
@@ -1,12 +1,3 @@
-//Template.course.events({
-//    'click  #edit-course': function () {
-//        // set edit mode variable to true
-//    },
-//    'click #cancel-course-edit': function () {
-//        // set edit mode variable to false
-//    }
-//});
-
 Template.course.rendered = function () {
     // set default mode for x-editable
     //$.fn.editable.defaults.mode = 'inline';
