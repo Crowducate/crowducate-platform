32df8c3b7bffcebf7b4d066493b0ad7cb9a24989
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-02-17 20:02:20 +0200

    Added published status to Course seed.

--- server/seeds/courses.js
+++ server/seeds/courses.js
@@ -7,7 +7,8 @@ Meteor.startup(function() {
                 'author': 'Illya Nziyev',
                 'date': " 29 Tammi 2015",
                 'about': "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris. Maecenas congue ligula ac quam viverra nec consectetur ante hendrerit. Donec et mollis dolor. Praesent et diam eget libero egestas mattis sit amet vitae augue.",
-                keywords: ['birds', 'bees', 'animals', 'insects']
+                'keywords': ['birds', 'bees', 'animals', 'insects'],
+                'published': "Published"
             }
         ];
 
