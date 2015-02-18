e7af8630c90bc6a65c88e5dba190fb299cb3c188
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-02-18 13:25:05 +0200

    Added created function, reactive vars inside.

--- client/templates/course/reactive-vars.js
+++ client/templates/course/reactive-vars.js
@@ -1,3 +1,8 @@
-// Keep track of active lesson
-// used in lesson template, sidebar, ..?
-activeLesson = new ReactiveVar();
+Template.course.created = function () {
+    // keep track of whether user is editing the lesson text
+    // Used in child templates such as lesson and sidebar
+    editingLessonText = new ReactiveVar(false);
+
+    // Set the empty active lesson ID variable
+    activeLessonID = new ReactiveVar();
+}
