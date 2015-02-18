eb8816a2c9373cd131532be993298a62721d738a
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-02-18 18:29:26 +0200

    editing course reactive var.

--- client/templates/course/reactive-vars.js
+++ client/templates/course/reactive-vars.js
@@ -1,4 +1,7 @@
 Template.course.created = function () {
+    // Determine if course is in edit mode
+    editingCourse = new ReactiveVar(false);
+
     // keep track of whether user is editing the lesson text
     // Used in child templates such as lesson and sidebar
     editingLessonText = new ReactiveVar(false);
