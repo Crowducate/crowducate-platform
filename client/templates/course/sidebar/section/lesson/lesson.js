4bcfdaf6e0f86fee498ea41d1c274fe66c99d8bd
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-02-18 13:47:08 +0200

    Added safety conditional, check edit mode.

--- client/templates/course/sidebar/section/lesson/lesson.js
+++ client/templates/course/sidebar/section/lesson/lesson.js
@@ -6,8 +6,22 @@ Template.sectionLesson.events({
     'click .sidebar-lesson-link': function (event) {
         event.preventDefault();
 
-        // set active lesson ID reactive variable
-        activeLessonID.set(this._id);
+        // Check if user is editing lesson text
+        if (editingLessonText.get()) {
+            // if so, alert them that changes might be lost
+            if (window.confirm("Changes may be lost! Proceed?")) {
+                // Remove rich text and set edit mode to false
+                cancelLessonEdit();
+
+                // set active lesson ID reactive variable
+                // to the value of clicked lesson
+                activeLessonID.set(this._id);
+            }
+        } else {
+            // set active lesson ID reactive variable
+            // to the value of clicked lesson
+            activeLessonID.set(this._id);
+        }
     },
     'click .editable-submit': function (event, template) {
         // Get the value of the inline editor
