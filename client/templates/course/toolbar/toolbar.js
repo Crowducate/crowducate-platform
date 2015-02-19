de5d891ea3b482b1e642f5dda94627dc3bb5cc57
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-02-19 14:52:48 +0200

    Removed unused rendered function.

--- client/templates/course/toolbar/toolbar.js
+++
@@ -1,6 +0,0 @@
-Template.courseToolbar.rendered = function () {
-    if (Session.get('editMode')) {
-        // Enable inline editors on sidebar items
-        enableSidebarInlineEditors();
-    }
-};
