601ebda5f537ee0b002eef278e70bbdb00aa9ff8
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-02-23 16:53:02 +0200

    Commented onAfterAction to remove strange error.

--- both/controllers/courseInfo.js
+++ both/controllers/courseInfo.js
@@ -8,13 +8,22 @@ CourseInfoController = AppController.extend({
         return Courses.findOne();
     },
     onAfterAction: function () {
+        //TODO: set meta course title without strange logged errors.
+        // specifically, uncommenting the following code produces some
+        // errors in console on page load.
+
+        // This logs the id, after an initial error
+        // the data must not be initially available
+        //console.log(this.data()._id);
+
+        //console.log(data);
         // Get the course ID from the URL parameters
-        var courseID = this.params._id;
+        //var courseID = this.params._id;
 
         // Get the course from the database
-        var course  = Courses.find(courseID).fetch()[0]; // select the zeroeth array item
+        //var course  = Courses.find(courseID).fetch()[0]; // select the zeroeth array item
 
         // Set the site title for SEO
-        Meta.setTitle(course.title);
+        //Meta.setTitle(course.title);
     }
 });
