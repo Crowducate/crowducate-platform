c2b4afec11ee905aa3c6e957b8c169234e9d189b
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-02-25 19:43:32 +0200

    Consolidated comments.

--- client/templates/shared/course-card.js
+++ client/templates/shared/course-card.js
@@ -3,18 +3,13 @@ Template.courseCard.helpers({
         /*
         Truncate keywords to a set limit
         Return an object with three attributes
-            truncated keywords
-            remaining keywords
-            count of remaining keywords
+            truncated keywords - array of keywords up to truncate limit
+            remaining keywords - array of keywords excluded by truncate
+            count of remaining keywords - numeric count of keywords excluded by truncate
         */
         var truncatedKeywordsObject = {
-            // get the first keywords up to the truncate limit
             'truncatedKeywords': keywords.slice(0, truncate),
-
-            // get the last keywords starting from the truncate point
             'remainingKeywords': keywords.slice(truncate),
-
-            // subtract the truncate value from the keywords length
             'remainingKeywordCount': keywords.length - truncate
         };
 
