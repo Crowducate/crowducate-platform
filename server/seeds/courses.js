e6ff59fb046245f93d9fefba8933982e2633e619
nizyev <illya.nizyev@gmail.com>
2015-03-02 13:44:55 +0200

    changed a default course example title

--- server/seeds/courses.js
+++ server/seeds/courses.js
@@ -3,7 +3,7 @@ Meteor.startup(function() {
         console.log("Adding default courses.");
         var defaultCourses = [
             {
-                'title': 'Example content',
+                'title': 'Introduction to Metrics for Smart Cities',
                 'author': 'Illya Nizyev',
                 'date': " 29 Tammi 2015",
                 'about': "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris. Maecenas congue ligula ac quam viverra nec consectetur ante hendrerit. Donec et mollis dolor. Praesent et diam eget libero egestas mattis sit amet vitae augue.",
