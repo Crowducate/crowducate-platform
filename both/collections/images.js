0122c6a64090b63fb114d62cf93717f42f99876a
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-03-03 12:17:46 +0200

    Added descriptive comments.

--- both/collections/images.js
+++ both/collections/images.js
@@ -1,4 +1,8 @@
+
+// Create images filestore
 var imageFilesystemStorage = new FS.Store.FileSystem("images", {path: "images"});
+
+// Create images collection
 Images = new FS.Collection("images", {
     stores: [imageFilesystemStorage]
 });
