56238b205c330d726a9fb811885ff3d8d450f1ef
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-03-16 13:16:23 +0200

    Extracted translation strings.

---
+++ server/mfExtracts.js
@@ -0,0 +1,124 @@
+mfPkg.addNative(
+{
+  "course add new header": {
+    "key": "course add new header",
+    "text": "Add new course",
+    "file": "./client/templates/teach/teach.html",
+    "line": 4,
+    "template": "teach",
+    "ctime": 1426503739075,
+    "mtime": 1426503739075
+  },
+  "add new course form course title label": {
+    "key": "add new course form course title label",
+    "text": "Title",
+    "file": "./client/templates/teach/teach.html",
+    "line": 8,
+    "template": "teach",
+    "ctime": 1426503739075,
+    "mtime": 1426503739075
+  },
+  "add new course form cover photo label": {
+    "key": "add new course form cover photo label",
+    "text": "Cover Photo",
+    "file": "./client/templates/teach/teach.html",
+    "line": 12,
+    "template": "teach",
+    "ctime": 1426503739075,
+    "mtime": 1426503739075
+  },
+  "add new course form author label": {
+    "key": "add new course form author label",
+    "text": "Author",
+    "file": "./client/templates/teach/teach.html",
+    "line": 16,
+    "template": "teach",
+    "ctime": 1426503739075,
+    "mtime": 1426503739075
+  },
+  "add new course form author placeholder": {
+    "key": "add new course form author placeholder",
+    "text": "Author Name",
+    "file": "./client/templates/teach/teach.html",
+    "line": 20,
+    "template": "teach",
+    "ctime": 1426503739075,
+    "mtime": 1426503739075
+  },
+  "add new course form keywords label": {
+    "key": "add new course form keywords label",
+    "text": "Keywords",
+    "file": "./client/templates/teach/teach.html",
+    "line": 23,
+    "template": "teach",
+    "ctime": 1426503739075,
+    "mtime": 1426503739075
+  },
+  "add new course form keywords placeholder": {
+    "key": "add new course form keywords placeholder",
+    "text": "Choose some tags (e.g. JavaScript, Math)",
+    "file": "./client/templates/teach/teach.html",
+    "line": 27,
+    "template": "teach",
+    "ctime": 1426503739075,
+    "mtime": 1426503739075
+  },
+  "add new course form published label": {
+    "key": "add new course form published label",
+    "text": "Change to \"Published\" when course should be viewable to others.",
+    "file": "./client/templates/teach/teach.html",
+    "line": 32,
+    "template": "teach",
+    "ctime": 1426503739075,
+    "mtime": 1426503739075
+  },
+  "add new course form unpublished option": {
+    "key": "add new course form unpublished option",
+    "text": "Unpublished",
+    "file": "./client/templates/teach/teach.html",
+    "line": 35,
+    "template": "teach",
+    "ctime": 1426503739075,
+    "mtime": 1426503739075
+  },
+  "add new course form published option": {
+    "key": "add new course form published option",
+    "text": "Published",
+    "file": "./client/templates/teach/teach.html",
+    "line": 36,
+    "template": "teach",
+    "ctime": 1426503739075,
+    "mtime": 1426503739075
+  },
+  "add new course form about course label": {
+    "key": "add new course form about course label",
+    "text": "About this course",
+    "file": "./client/templates/teach/teach.html",
+    "line": 40,
+    "template": "teach",
+    "ctime": 1426503739075,
+    "mtime": 1426503739075
+  },
+  "add new course form about course help text": {
+    "key": "add new course form about course help text",
+    "text": "Briefly describe the objectives of this course.",
+    "file": "./client/templates/teach/teach.html",
+    "line": 43,
+    "template": "teach",
+    "ctime": 1426503739075,
+    "mtime": 1426503739075
+  },
+  "add new course form add course button": {
+    "key": "add new course form add course button",
+    "text": "Add Course",
+    "file": "./client/templates/teach/teach.html",
+    "line": 45,
+    "template": "teach",
+    "ctime": 1426503739075,
+    "mtime": 1426503739075
+  }
+},
+{
+  "extractedAt": 1426504531508,
+  "updatedAt": 1426503739075
+});
