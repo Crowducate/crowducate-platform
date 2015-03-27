f671eacf92da27fbca113b659c4b083c92a0f128
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-03-27 15:54:08 +0200

    Added Free Culture helper.

---
+++ client/templates/license/questions/questions.js
@@ -0,0 +1,17 @@
+Template.licenseQuestions.helpers({
+  'attribution': function () {
+    return true;
+  },
+  'shareAlike': function () {
+    return true;
+  },
+  'allowAdaptation': function () {
+    return true;
+  },
+  'allowCommercial': function () {
+    return true;
+  },
+  'freeCulture': function () {
+    return true;
+  }
+});
