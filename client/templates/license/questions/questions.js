0fcba26997995bbd2d2532392a5b89ad549f67d5
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-03-31 11:51:18 +0300

    Reactive variable defaults; template events; adaptations switch.

--- client/templates/license/questions/questions.js
+++ client/templates/license/questions/questions.js
@@ -6,7 +6,23 @@ Template.licenseQuestions.helpers({
     return true;
   },
   'allowAdaptation': function () {
-    return true;
+    // Hide the 'no derivative works' icon if adaptations are allow
+
+    // get the value of CC Adaptation reactive variable
+    allowAdaptation = ccAdaptation.get();
+
+    // Make sure adaptation is 'yes' or 'share-alike'
+    switch (allowAdaptation) {
+      case 'yes':
+        return true;
+        break;
+      case 'share-alike':
+        return true;
+        break;
+      case 'no':
+        return false;
+        break;
+    }
   },
   'allowCommercial': function () {
     return true;
@@ -18,7 +34,19 @@ Template.licenseQuestions.helpers({
 
 Template.licenseQuestions.created = function () {
   // Creative Commons license reactive variables
-  var ccAdaptation = new ReactiveVar(),
-      ccCommercial = new ReactiveVar();
+  // Default values set to match template dafaults
+  ccAdaptation = new ReactiveVar('yes');
+  ccCommercial = new ReactiveVar('yes');
 
-}
+};
+
+Template.licenseQuestions.events({
+  'change [name=cc-adaptation]': function (event, template) {
+    // Set the value of CC Adaptation reactive variable
+    ccAdaptation.set(event.target.value);
+  },
+  'change [name=cc-commercial]': function (event, template) {
+    // Set the value of CC Commercial reactive variable
+    ccCommercial.set(event.target.value);
+  }
+});
