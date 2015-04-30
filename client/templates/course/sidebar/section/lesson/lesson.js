e3d6902113e1504929cba2f4587c70029e1663a9
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-04-30 13:52:26 +0300

    Template helper and reactive variable for editing course.

--- client/templates/course/sidebar/section/lesson/lesson.js
+++ client/templates/course/sidebar/section/lesson/lesson.js
@@ -1,3 +1,40 @@
+Template.sectionLesson.created = function () {
+    // Create new reactive variable for course edit state
+    this.editingCourseVar = new ReactiveVar(false);
+
+    // Get the current router
+    controller = Router.current();
+
+    // Get current course from router
+    currentCourseId = controller.params._id;
+
+    /*
+    * when user is editing the active course,
+    * set the value of a reactive variable (true/false)
+    * Used to show/hide wysiwyg editor.
+    */
+    this.autorun(function () {
+        var instance = Template.instance();
+
+        // Update course edit variable when editing course
+        if (currentCourseId === Session.get('editingCourseID')) {
+            instance.editingCourseVar.set(true);
+        } else {
+            instance.editingCourseVar.set(false);
+        };
+    });
+};
+
+Template.sectionLesson.helpers({
+    'editingCourse': function () {
+        // Get template instance
+        var instance = Template.instance();
+
+        // Keep track of whether user is editing course
+        return instance.editingCourseVar.get();
+    }
+});
+
 Template.sectionLesson.events({
     'click .sidebar-lesson-link': function (event) {
         event.preventDefault();
@@ -18,12 +55,5 @@ Template.sectionLesson.events({
             // to the value of clicked lesson
             activeLessonID.set(this._id);
         }
-    },
-    'click .editable-submit': function (event, template) {
-        // Get the new name from the inline editor
-        var newName = template.find('input').value;
-
-        // update the lesson in database
-        Lessons.update(this._id, {$set: {'name': newName}});
     }
 });
