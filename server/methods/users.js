5da0f3985df0a5b96e7722d321f4a2b5263109d3
sbe <sbe@@users.noreply.github.com>
2015-04-30 17:28:10 +0200

    Fields now update to the users collection

--- server/methods/users.js
+++ server/methods/users.js
@@ -2,19 +2,24 @@ Meteor.methods({
     'User.update': function (userId,target,doc) {
         if (Meteor.userId() == userId && target && doc)
         {
+            console.log(userId + ":" + target + ":" + doc);
             console.log("user okay, target and doc not null");
             switch(target) {
                 case "username":
                     console.log("Target is username");
+                    Meteor.users.update(userId, {$set: {'username': doc}});
                     break;
                 case "realname":
                     console.log("Target is realname");
+                    Meteor.users.update(userId, {$set: {'realname': doc}});
                     break;
                 case "gender":
                     console.log("Target is Gender");
+                    Meteor.users.update(userId, {$set: {'gender': doc}});
                     break;
                 case "languages":
                     console.log("Target is Languages");
+                    Meteor.users.update(userId, {$set: {'languages': doc}});
                     break;
 
                 default:
