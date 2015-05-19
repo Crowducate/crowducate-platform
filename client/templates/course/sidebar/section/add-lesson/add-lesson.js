585b8afe53f5e682849ad9cc77917531428c09b6
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-05-19 15:23:14 +0300

    Add lesson, and link to section.

--- client/templates/course/sidebar/section/add-lesson/add-lesson.js
+++ client/templates/course/sidebar/section/add-lesson/add-lesson.js
@@ -3,30 +3,20 @@ Template.addLesson.events({
         // Get lesson name
         var lessonName = template.find("#lesson-name").value;
 
-        // Get course ID from parent template
-        var courseID = Template.parentData()._id;
+        // Get section ID from current data context
+        var sectionID = String(this);
 
         // Create temporary lesson object
         var lessonObject = {
-            'name': lessonName,
-            'courseIDs': [courseID] // add course ID to lesson object, for collection helper
+            'name': lessonName
         };
 
         // Add lesson to database,
         // getting lesson ID in return
         var lessonId = Lessons.insert(lessonObject);
-        if (!this.lessonIDs) {
-            this.lessonIDs = [];
-        }
 
-        // Add lesson ID to array
-        this.lessonIDs.push(lessonId);
-
-        // Get course sections array from parent template
-        var courseSections = Template.parentData().sections;
-
-        // Save course.lessonIDs array to database
-        Courses.update(courseID, {$set: {"sections": courseSections}});
+        // Add lesson ID to section record
+        Sections.update(sectionID, {$push: {'lessonIDs': lessonId}});
 
         // Clear the lesson name field
         $("#lesson-name").val('');
