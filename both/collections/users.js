0e2ba08268eb58de57042ebe4b88dd319f508827
sbe <sbe@@users.noreply.github.com>
2015-05-22 21:23:23 +0200

    adding aldeed packages

---
+++ both/collections/users.js
@@ -0,0 +1,39 @@
+Schema = {};
+
+Schema.UserProfile = new SimpleSchema({
+	realName: {
+		type: String,
+		regEx: /^[a-zA-Z-]{2,25}$/,
+		optional: false
+	},
+	gender: {
+		type: String
+	},
+	language: {
+		type: String
+	
+
+});
+
+Schema.User = new SimpleSchema({
+	username: {
+		type: String,
+		regEx: /^[a-zA-Z-]{2,25}$/,	
+	},
+	emails: {
+		type: [Object]
+	},
+	"emails.$.address": {
+		type: String,
+		regEx: SimpleSchema.RegEx.Email
+	},
+	createdAt: {
+		type: Date
+	},
+	profile: {
+		type: Schema.UserProfile,
+		optional: true
+	}
+});
+
+Meteor.users.attachSchema(Schema.User);
