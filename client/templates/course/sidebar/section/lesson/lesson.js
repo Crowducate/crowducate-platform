de61f622a30ff944ad7813fc9ccde65e18311fb2
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-05-22 12:59:43 +0300

    Indentation.

--- client/templates/course/sidebar/section/lesson/lesson.js
+++ client/templates/course/sidebar/section/lesson/lesson.js
@@ -1,59 +1,77 @@
 Template.sectionLesson.created = function () {
-    // Create new reactive variable for course edit state
-    this.editingCourseVar = new ReactiveVar(false);
+  // Create new reactive variable for course edit state
+  this.editingCourseVar = new ReactiveVar(false);
 
-    // Get the current router
-    controller = Router.current();
+  // Save lesson ID as instance variable
+  this.lessonID = this.data;
 
-    // Get current course from router
-    currentCourseId = controller.params._id;
+  // Subscribe to single section lesson
+  this.subscribe('singleLesson', this.lessonID);
 
-    /*
-    * when user is editing the active course,
-    * set the value of a reactive variable (true/false)
-    * Used to show/hide wysiwyg editor.
-    */
-    this.autorun(function () {
-        var instance = Template.instance();
+  // Get the current router
+  controller = Router.current();
 
-        // Update course edit variable when editing course
-        if (currentCourseId === Session.get('editingCourseID')) {
-            instance.editingCourseVar.set(true);
-        } else {
-            instance.editingCourseVar.set(false);
-        };
-    });
+  // Get current course from router
+  currentCourseId = controller.params._id;
+
+  /*
+  * when user is editing the active course,
+  * set the value of a reactive variable (true/false)
+  * Used to show/hide wysiwyg editor.
+  */
+  this.autorun(function () {
+    var instance = Template.instance();
+
+    // Update course edit variable when editing course
+    if (currentCourseId === Session.get('editingCourseID')) {
+      instance.editingCourseVar.set(true);
+    } else {
+      instance.editingCourseVar.set(false);
+    };
+  });
 };
 
 Template.sectionLesson.helpers({
-    'editingCourse': function () {
-        // Get template instance
-        var instance = Template.instance();
+  'editingCourse': function () {
+    // Get template instance
+    var instance = Template.instance();
 
-        // Keep track of whether user is editing course
-        return instance.editingCourseVar.get();
-    }
+    // Keep track of whether user is editing course
+    return instance.editingCourseVar.get();
+  },
+  'lesson': function () {
+    // Get template instance as variable
+    var instance = Template.instance();
+
+    // Get lesson ID from template instance
+    var lessonID = instance.lessonID;
+
+    // Get lesson object from database
+    var lessonObject = Lessons.findOne(lessonID);
+
+    return lessonObject;
+  }
 });
 
 Template.sectionLesson.events({
-    'click .sidebar-lesson-link': function (event) {
-        event.preventDefault();
+  'click .sidebar-lesson-link': function (event) {
+    event.preventDefault();
 
-        // Check if user is editing lesson text
-        if (editingLessonText.get()) {
-            // if so, alert them that changes might be lost
-            if (window.confirm("Changes may be lost! Proceed?")) {
-                // Remove rich text and set edit mode to false
-                lessonEditCleanup();
+    // Check if user is editing lesson text
+    if (editingLessonText.get()) {
+      // if so, alert them that changes might be lost
+      if (window.confirm("Changes may be lost! Proceed?")) {
+        // Remove rich text and set edit mode to false
+        lessonEditCleanup();
 
-                // set active lesson ID reactive variable
-                // to the value of clicked lesson
-                activeLessonID.set(this._id);
-            }
-        } else {
-            // set active lesson ID reactive variable
-            // to the value of clicked lesson
-            activeLessonID.set(this._id);
-        }
+        // set active lesson ID reactive variable
+        // to the value of clicked lesson
+        activeLessonID.set(this._id);
+      }
+    } else {
+      // set active lesson ID reactive variable
+      // to the value of clicked lesson
+      activeLessonID.set(this._id);
     }
+  }
 });
