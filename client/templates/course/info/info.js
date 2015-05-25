0fff540c2865c4d16d3fa661ae5d7c8ceb81d989
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-05-25 13:16:33 +0300

    Use editingThisCourseVar. Cleanup.

--- client/templates/course/info/info.js
+++ client/templates/course/info/info.js
@@ -1,34 +1,6 @@
-Template.courseInfo.created = function(){
-  // Create new reactive variable for course edit state
-  this.editingCourseVar = new ReactiveVar(false);
-
-  // Get the current router
-  controller = Router.current();
-
-  // Get current course from router
-  currentCourse = controller.params._id;
-
-  /*
-  * when user is editing the active course,
-  * set the value of a reactive variable (true/false)
-  * Used to show/hide wysiwyg editor.
-  */
-  this.autorun(function () {
-      var instance = Template.instance();
-
-      // Update course edit variable when editing course
-      if (currentCourse === Session.get('editingCourseID')) {
-          instance.editingCourseVar.set(true);
-      } else {
-          instance.editingCourseVar.set(false);
-      };
-  });
-};
-
 Template.courseInfo.helpers({
-  'editingThisCourse': function () {
-      var instance = Template.instance();
-      // Keep track of whether user is editing course
-      return instance.editingCourseVar.get();
+  'editingThisCourse': function (event, template) {
+    // return true if user is editing this course
+    return editingThisCourseVar.get();
   }
 });
