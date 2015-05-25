97cc18620a0fa143712f6135d8322089ede5e964
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-05-25 12:14:13 +0300

    Use editingThisCourse reactive variable.

--- client/templates/course/sidebar/section/section.js
+++ client/templates/course/sidebar/section/section.js
@@ -1,24 +1,21 @@
 Template.courseSidebarSection.helpers({
-    'editingThisCourse': function () {
-        // Get the course ID from parent template
-        var courseID = Template.parentData()._id;
+  'editingThisCourse': function () {
+    // Return the boolean value of editing this course reactive variable
+    return editingThisCourseVar.get();
+  },
+  'section': function () {
+    // Get the section ID from template data
+    var sectionID = String(this);
 
-        // return true if editing this course
-        return (Session.get('editingCourseID') === courseID);
-    },
-    'section': function () {
-      // Get the section ID from template data
-      var sectionID = String(this);
-
-      // Make sure template subscription is ready
-      if (Template.instance().subscriptionsReady()) {
-        // Get the section from database
-        var section = Sections.findOne(sectionID);
+    // Make sure template subscription is ready
+    if (Template.instance().subscriptionsReady()) {
+      // Get the section from database
+      var section = Sections.findOne(sectionID);
 ;
-        // Get the section from template level subscription
-        return section;
-      }
+      // Get the section from template level subscription
+      return section;
     }
+  }
 });
 
 Template.courseSidebarSection.onCreated(function () {
