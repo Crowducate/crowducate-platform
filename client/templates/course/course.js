1fb06489ef0b9878a47c3288377b4079ae211901
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-05-25 13:15:41 +0300

    Moved editingThisCourseVar.

--- client/templates/course/course.js
+++ client/templates/course/course.js
@@ -1,22 +1,4 @@
 Template.course.created = function () {
-    // keep track of whether user is editing this course
-    // Used in child templates such as lesson and sidebar
-    editingThisCourseVar = new ReactiveVar(false);
-
     // Set the empty active lesson ID variable
     activeLessonID = new ReactiveVar(undefined);
-
-    // Get the current router object
-    var controller = Router.current();
-
-    // Get course ID from router object
-    var courseID = controller.params._id;
-
-    this.autorun(function () {
-      if (Session.get('editingCourseID') === courseID) {
-        editingThisCourseVar.set(true);
-      } else {
-        editingThisCourseVar.set(false);
-      };
-    });
 };
--- client/templates/course/toolbar/toolbar.js
+++ client/templates/course/toolbar/toolbar.js
@@ -1,6 +1,26 @@
 Template.courseToolbar.helpers({
-    'editMode': function (event, template) {
-        // return true if user is editing this course
-        return (Session.get('editingCourseID') === this._id);
-    }
+  'editingThisCourse': function (event, template) {
+    // return true if user is editing this course
+    return editingThisCourseVar.get();
+  }
 });
+
+Template.courseToolbar.created = function () {
+  // keep track of whether user is editing this course
+  // Used in child templates such as lesson and sidebar
+  editingThisCourseVar = new ReactiveVar(false);
+
+  // Get the current router object
+  var controller = Router.current();
+
+  // Get course ID from router object
+  var courseID = controller.params._id;
+
+  this.autorun(function () {
+    if (Session.get('editingCourseID') === courseID) {
+      editingThisCourseVar.set(true);
+    } else {
+      editingThisCourseVar.set(false);
+    };
+  });
+}
