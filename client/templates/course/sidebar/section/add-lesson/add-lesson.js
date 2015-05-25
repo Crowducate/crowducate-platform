54c2cf732972600b9f0cc05d31318ce32de5f434
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-05-25 12:19:39 +0300

    Form and submit event.

--- client/templates/course/sidebar/section/add-lesson/add-lesson.js
+++ client/templates/course/sidebar/section/add-lesson/add-lesson.js
@@ -1,24 +1,26 @@
 Template.addLesson.events({
-    'click .add-lesson-button': function (event, template) {
-        // Get lesson name
-        var lessonName = template.find("#lesson-name").value;
+    'submit #add-lesson-form': function (event, template) {
+      event.preventDefault();
 
-        // Get section ID from current data context
-        var sectionID = String(this);
+      // Get lesson name
+      var lessonName = template.find(".lesson-name").value;
 
-        // Create temporary lesson object
-        var lessonObject = {
-            'name': lessonName
-        };
+      // Get section ID from current data context
+      var sectionID = String(this);
 
-        // Add lesson to database,
-        // getting lesson ID in return
-        var lessonId = Lessons.insert(lessonObject);
+      // Create temporary lesson object
+      var lessonObject = {
+        'name': lessonName
+      };
 
-        // Add lesson ID to section record
-        Sections.update(sectionID, {$push: {'lessonIDs': lessonId}});
+      // Add lesson to database,
+      // getting lesson ID in return
+      var lessonId = Lessons.insert(lessonObject);
 
-        // Clear the lesson name field
-        $("#lesson-name").val('');
-    }
+      // Add lesson ID to section record
+      Sections.update(sectionID, {$push: {'lessonIDs': lessonId}});
+
+      // Clear the lesson name field
+      $(".lesson-name").val('');
+  }
 });
