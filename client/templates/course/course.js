c9fd08a82c5605d6647a84c347107fed4ab47ebf
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-05-25 12:09:14 +0300

    Added editing this course reactive variable.

--- client/templates/course/course.js
+++ client/templates/course/course.js
@@ -1,8 +1,24 @@
 Template.course.created = function () {
-    // keep track of whether user is editing the lesson text
+    // keep track of whether user is editing this course
     // Used in child templates such as lesson and sidebar
-    editingLessonText = new ReactiveVar(false);
+    editingThisCourseVar = new ReactiveVar(false);
 
     // Set the empty active lesson ID variable
     activeLessonID = new ReactiveVar(undefined);
+
+    // Get the current router object
+    var controller = Router.current();
+
+    // Get course ID from router object
+    var courseID = controller.params._id;
+
+    this.autorun(function () {
+      if (Session.get('editingCourseID') === courseID) {
+        editingThisCourseVar.set(true);
+        console.log(editingThisCourseVar.get());
+      } else {
+        editingThisCourseVar.set(false);
+        console.log(editingThisCourseVar.get());
+      };
+    });
 };
