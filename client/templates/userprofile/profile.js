8d4bf9221aab3e26aa34bb5d7b7c36ece0b460a1
sbe <sbe@@users.noreply.github.com>
2015-05-26 18:52:10 +0200

    Implementing Bio and Dropdown functionality

--- client/templates/userprofile/profile.js
+++ client/templates/userprofile/profile.js
@@ -1,5 +1,9 @@
 Template.profile.helpers({
 	'profile': function() {
 		return Meteor.users.findOne({'id':this._id});
+	},
+	'firstname': function() {
+		var first = Meteor.user().realname.split(' ');
+		return first[0];
 	}
-})
+});
