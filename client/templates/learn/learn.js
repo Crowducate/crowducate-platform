43ed875f42af07c55ec3beca53506122baa1e2c1
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-09-08 13:14:50 +0300

    Add publishedCourses subscription

--- client/templates/learn/learn.js
+++ client/templates/learn/learn.js
@@ -2,4 +2,12 @@ Template.learn.helpers({
     'courses': function(){
         return Courses.find();
     }
-})
+});
+
+Template.learn.created = function () {
+  // Get reference to template instance
+  var instance = this;
+
+  // Subscribe to all published courses
+  instance.subscribe("publishedCourses");
+};
