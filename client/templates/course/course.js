e9226a54658da17aed4113b3eb8db21d8ed61b22
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-09-11 11:31:48 +0300

    Add singleCourse subscription; courseId variable

--- client/templates/course/course.js
+++ client/templates/course/course.js
@@ -1,4 +1,16 @@
 Template.course.created = function () {
-    // Set the empty active lesson ID variable
-    activeLessonID = new ReactiveVar(undefined);
+  // Get reference to template instance
+  var instance = this;
+
+  // Get reference to router
+  var router = Router.current();
+
+  // Get course ID from router
+  instance.courseId = router.params._id;
+
+  // Subscribe to single course
+  instance.subscribe('singleCourse', instance.courseId);
+
+  // Set the empty active lesson ID variable
+  activeLessonID = new ReactiveVar(undefined);
 };
