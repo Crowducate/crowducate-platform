51276877c0e8d3f6ba52bc8210ca8bf9ee47e3ce
sbe <sbe@users.noreply.github.com>
2015-09-13 19:44:53 +0200

    reverting to former state

--- client/templates/course/toolbar/edit-course.js
+++ client/templates/course/toolbar/edit-course.js
@@ -10,26 +10,3 @@ Template.editCourse.events({
     Session.set('editingCourseId', courseId);
   }
 });
-
-Template.editCourse.helpers({
-	'canEditCourse': function() {
-    //
-		// var router = Router.current();
-    //
-		// var courseId = router.params._id;
-    //
-		// //TODO: fine grain
-		// var course = Courses.findOne({_id: courseId});
-    //
-		// //TODO solve exceptions, which are most likely resulting from data not being available early enough.
-		// if (Meteor.user().username == course.owner)
-		// {
-		// 	return true;
-		// }
-		// else
-		// {
-		// 	return false;
-		// }
-    //
-	}
-});
