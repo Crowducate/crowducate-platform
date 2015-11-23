a9661dbe84ff5da734926d6183b4323e03119ffa
sbe <sbe@sbedev.com>
2015-11-23 15:02:25 +0100

    added a return false to the onSubmit function of passwordEdit

--- client/templates/userprofile/settings.js
+++ client/templates/userprofile/settings.js
@@ -4,14 +4,15 @@ AutoForm.hooks({
             this.event.preventDefault();
             Accounts.changePassword(insertDoc.old, insertDoc.new, function(error) {
                 if (error) {
-                    sAlert.error(error);
+                    sAlert.error(error.message);
+                    this.done(error.message);
                 } else {
-                    sAlert.success("Password successfully changed.")
-
+                    sAlert.success("Password successfully updated.")
+                    AutoForm.resetForm('passwordEdit');
+                    this.done("Password updated.")
                 }
             });
-            this.resetForm();
-
+            return false
         }
     }
 });
