47336cb435ea0f3267359801d05a3ba28787afdc
sbe <sbe@sbedev.com>
2015-11-23 14:32:56 +0100

    Added sAlert options

---
+++ client/config/sAlert.js
@@ -0,0 +1,27 @@
+Meteor.startup(function () {
+
+    sAlert.config({
+        effect: '',
+        position: 'top',
+        timeout: 5000,
+        html: false,
+        onRouteClose: true,
+        stack: true,
+        // or you can pass an object:
+        // stack: {
+        //     spacing: 10 // in px
+        //     limit: 3 // when fourth alert appears all previous ones are cleared
+        // }
+        offset: 0, // in px - will be added to first alert (bottom or top - depends of the position in config)
+        beep: false
+        // examples:
+        // beep: '/beep.mp3'  // or you can pass an object:
+        // beep: {
+        //     info: '/beep-info.mp3',
+        //     error: '/beep-error.mp3',
+        //     success: '/beep-success.mp3',
+        //     warning: '/beep-warning.mp3'
+        // }
+    });
+
+});
