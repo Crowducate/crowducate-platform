c42a1d72e1e613d34b29c6e6d0c303a8a1d81dbf
sbe <sbe@sbedev.com>
2015-11-26 11:14:22 +0100

    code style edits

--- server/methods/users.js
+++ server/methods/users.js
@@ -1,13 +1,17 @@
 Meteor.methods({
-    'User.update': function (modifier,documentId) {
-        check(modifier,Schema.User);
-        Meteor.users.update(documentId, modifier);
-    },
-    'changeUserPassword': function(doc) {
-    	//TODO Refactor to submit form without method
-        check(doc,Schema.User);
-    },
-    'User.biography.update': function(userId,doc) {
-    	 Meteor.users.update(userId, {$set: {'profile.biography': doc}});
-    }
+  'User.update': function(modifier, documentId) {
+    check(modifier, Schema.User);
+    Meteor.users.update(documentId, modifier);
+  },
+  'changeUserPassword': function(doc) {
+    //TODO Refactor to submit form without method
+    check(doc, Schema.User);
+  },
+  'User.biography.update': function(userId, doc) {
+    Meteor.users.update(userId, {
+      $set: {
+        'profile.biography': doc
+      }
+    });
+  }
 });
