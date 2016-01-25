d2dc8f4be5d9d166b328080a0b936ad48705a3ba
sebbel <sebastian@pancia.de>
2016-01-25 15:18:44 +0100

    removing this.autorun from subscription

--- client/templates/courses/tagged-courses.js
+++ client/templates/courses/tagged-courses.js
@@ -10,7 +10,5 @@ Template.taggedCourses.helpers({
 Template.taggedCourses.onCreated(function(){
   var controller = Iron.controller();
   var params = controller.getParams();
-  this.autorun(function(){
-    Meteor.subscribe('taggedCourses', params.tag)
-  });
+  Meteor.subscribe('taggedCourses', params.tag)
 });
