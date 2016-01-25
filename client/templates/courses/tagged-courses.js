9cfcfc64fd0cae8436b3b675ae3742a95f6dff8c
sebbel <sebastian@pancia.de>
2016-01-25 15:21:35 +0100

    Comments

--- client/templates/courses/tagged-courses.js
+++ client/templates/courses/tagged-courses.js
@@ -8,7 +8,10 @@ Template.taggedCourses.helpers({
 });
 
 Template.taggedCourses.onCreated(function(){
+  // Accessing the Iron.controller to invoke getParams method of Iron Router.
   var controller = Iron.controller();
+  // Getting Params of the URL
   var params = controller.getParams();
+  // Invoking subscription on the Courses with the :tag params
   Meteor.subscribe('taggedCourses', params.tag)
 });
